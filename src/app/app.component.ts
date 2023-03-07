e65eff87678ddb63f70acfb61890887e969886a2
Thiago Abreu <thiagoabreu72@gmail.com>
2023-03-07 10:05:31 -0300

    exemplo para conexão de dados com o Senior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { user } from '@seniorsistemas/senior-platform-data';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'senior';
+  token: any;
+  nome: string = '';
+
+  constructor() {
+    user
+      .getToken()
+      .then((data) => {
+        this.token = data;
+        this.nome = this.token.fullName.replaceAll('+', ' ');
+      })
+      .catch((error) => {
+        alert(
+          'Não foi possível obter token. Verifique se a tela está sendo acessada pela plataforma Senior X.'
+        );
+      });
+  }
 }
